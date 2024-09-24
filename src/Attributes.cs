a6354a432e651f0c3b8d6145b49e081da9571ba7
Newex <johnnykristianmoebergsoerensen@gmail.com>
2024-09-24 19:10:33 -0100

    feat: Update attributes to match v.0.0.18

--- src/Attributes.cs
+++ src/Attributes.cs
@@ -6,7 +6,7 @@ namespace PartialSourceGen;
 /// Generate partial optional properties of this class/struct.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-internal sealed class PartialAttribute : Attribute
+public sealed class PartialAttribute : Attribute
 {
     /// <summary>
     /// The optional summary for the partial entity. If not given
@@ -31,13 +31,23 @@ internal sealed class PartialAttribute : Attribute
     /// entity. Default false.
     /// </summary>
     public bool IncludeExtraAttributes { get; set; }
+
+    /// <summary>
+    /// Removes the abstract keyword in the partial entity.
+    /// </summary>
+    public bool RemoveAbstractModifier { get; set; }
+
+    /// <summary>
+    /// Set the derived type for the partial entity.
+    /// </summary>
+    public Type? DeriveFrom { get; set; }
 }
 
 /// <summary>
 /// Include the initializer for this property in the partial entity.
 /// </summary>
 [AttributeUsage(AttributeTargets.Property)]
-internal sealed class IncludeInitializerAttribute : Attribute
+public sealed class IncludeInitializerAttribute : Attribute
 {
 }
 
@@ -52,9 +62,24 @@ internal sealed class IncludeInitializerAttribute : Attribute
 /// </remarks>
 /// <param name="name">The partial property name to generate</param>
 [AttributeUsage(AttributeTargets.Property)]
-internal sealed class PartialReferenceAttribute<TOriginal, TPartial>(string? name = null) : Attribute
+public sealed class PartialReferenceAttribute<TOriginal, TPartial>(string? name = null) : Attribute
 {
     private readonly string? name = name;
+}
+/// <summary>
+/// Replace a type with the given type.
+/// </summary>
+/// <typeparam name="T">The type to use instead.</typeparam>
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class PartialTypeAttribute<T> : Attribute
+{
+    /// <summary>
+    /// Instantiate a partial type attribute.
+    /// </summary>
+    /// <param name="name">The partial property name to generate.</param>
+    public PartialTypeAttribute(string? name = null)
+    {
+    }
 }
 #endif
 /// <summary>
@@ -67,23 +92,38 @@ internal sealed class PartialReferenceAttribute<TOriginal, TPartial>(string? nam
 /// <param name="partial">The partial type</param>
 /// <param name="name">The partial property name to generate</param>
 [AttributeUsage(AttributeTargets.Property)]
-internal sealed class PartialReferenceAttribute(Type original, Type partial, string? name = null) : Attribute
+public sealed class PartialReferenceAttribute(Type original, Type partial, string? name = null) : Attribute
 {
     private readonly Type original = original;
     private readonly Type partial = partial;
     private readonly string? name = name;
 }
 /// <summary>
+/// Replace a type with the given type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class PartialTypeAttribute : Attribute
+{
+    /// <summary>
+    /// Instantiate a partial type attribute.
+    /// </summary>
+    /// <param name="replacement">The replacement type.</param>
+    /// <param name="name">The partial property name to generate.</param>
+    public PartialTypeAttribute(Type replacement, string? name = null)
+    {
+    }
+}
+/// <summary>
 /// Excludes a property from being included in the generated partial entity
 /// </summary>
 [AttributeUsage(AttributeTargets.Property)]
-internal sealed class ExcludePartialAttribute : Attribute
+public sealed class ExcludePartialAttribute : Attribute
 {
 }
 /// <summary>
 /// Force a property to be nullable
 /// </summary>
 [AttributeUsage(AttributeTargets.Property)]
-internal sealed class ForceNullAttribute : Attribute
+public sealed class ForceNullAttribute : Attribute
 {
 }
